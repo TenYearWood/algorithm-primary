a34ce1be2ea642d360e2683cace51639391aa3ee
chengyu <chengyu@csii.com.cn>
2025-04-12 14:36:23 +0800

    单链表

---
+++ src/main/java/com/cy/class04/Code01_ReverseList.java
@@ -0,0 +1,46 @@
+package com.cy.class04;
+
+/**
+ *
+ */
+public class Code01_ReverseList {
+
+    public static class Node {
+        public int value;
+        public Node next;
+
+        public Node(int data) {
+            this.value = data;
+        }
+    }
+
+    /**
+     * 单链表的逆序
+     */
+    public static Node reverseLinkedList(Node head) {
+        Node pre = null;
+        Node next = null;
+        while (head != null) {
+            next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+
+    public static void main(String[] args) {
+        Node n1 = new Node(1);
+        n1.next = new Node(2);
+        n1.next.next = new Node(3);
+
+        n1 = reverseLinkedList(n1);
+
+
+//        while(n1 != null) {
+//            System.out.print(n1.value + " ");
+//            n1 = n1.next;
+//        }
+//        System.out.println();
+    }
+}
