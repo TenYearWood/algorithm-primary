5527fe9ea925b118b2ea37e81acf554b04bfa362
chengyu <chengyu@csii.com.cn>
2025-04-14 21:37:40 +0800

    双链表的逆序

--- src/main/java/com/cy/class04/Code01_ReverseList.java
+++ src/main/java/com/cy/class04/Code01_ReverseList.java
@@ -14,6 +14,16 @@ public class Code01_ReverseList {
         }
     }
 
+    public static class DoubleNode {
+        public int value;
+        public DoubleNode last;
+        public DoubleNode next;
+
+        public DoubleNode(int data) {
+            this.value = data;
+        }
+    }
+
     /**
      * 单链表的逆序
      */
@@ -23,6 +33,19 @@ public class Code01_ReverseList {
         while (head != null) {
             next = head.next;
             head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+
+    public static DoubleNode reverseDoubleList(DoubleNode head) {
+        DoubleNode pre = null;
+        DoubleNode next = null;
+        while(head != null) {
+            next = head.next;
+            head.next = pre;
+            head.last = next;
             pre = head;
             head = next;
         }
