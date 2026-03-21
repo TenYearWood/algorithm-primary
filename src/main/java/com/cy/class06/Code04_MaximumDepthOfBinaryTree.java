ece06f429a5931615e4fa175c75e5d4c2478fa6a
chengyu <chengyu@csii.com.cn>
2026-03-21 14:42:03 +0800

    二叉树leecode 题目

---
+++ src/main/java/com/cy/class06/Code04_MaximumDepthOfBinaryTree.java
@@ -0,0 +1,22 @@
+package com.cy.class06;
+
+/**
+ * 测试链接：https://leetcode.com/problems/maximum-depth-of-binary-tree
+ * 返回一颗树的最大深度
+ */
+public class Code04_MaximumDepthOfBinaryTree {
+
+    public static class TreeNode {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+    }
+
+    // 以root为头的树，最大高度是多少返回！
+    public static int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
