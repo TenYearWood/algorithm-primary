486851c9a893257a65b6c581bb80ded8ee41cc3c
chengyu <chengyu@csii.com.cn>
2026-03-29 15:37:51 +0800

    归并排序，非递归方法实现

---
+++ src/main/java/com/cy/class08/Code02_MergeSort2.java
@@ -0,0 +1,67 @@
+package com.cy.class08;
+
+/**
+ * 归并排序，非递归方法实现
+ */
+public class Code02_MergeSort2 {
+
+    public static void mergeSort2(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        int step = 1;
+        int N = arr.length;
+        while (step < N) {
+            int L = 0;
+            while (L < N) {
+                int M = 0;
+                if (N - L >= step) {
+                    M = L + step - 1;
+                } else {
+                    M = N - 1;
+                }
+                if (M == N - 1) {
+                    break;
+                }
+                int R = 0;
+                if (N - 1 - M >= step) {
+                    R = M + step;
+                } else {
+                    R = N - 1;
+                }
+                merge(arr, L, M, R);
+                if (R == N - 1) {
+                    break;
+                } else {
+                    L = R + 1;
+                }
+            }
+            if (step > N / 2) {
+                break;
+            }
+            step *= 2;
+        }
+    }
+
+    public static void merge(int[] arr, int L, int M, int R) {
+        int[] help = new int[R - L + 1];
+        int i = 0;
+        int p1 = L;
+        int p2 = M + 1;
+        while (p1 <= M && p2 <= R) {
+            help[i++] = arr[p1] <= arr[p2] ? arr[p1++] : arr[p2++];
+        }
+        // 要么p1越界，要么p2越界
+        // 不可能出现：共同越界
+        while (p1 <= M) {
+            help[i++] = arr[p1++];
+        }
+        while (p2 <= R) {
+            help[i++] = arr[p2++];
+        }
+        for (i = 0; i < help.length; i++) {
+            arr[L + i] = help[i];
+        }
+    }
+
+}
